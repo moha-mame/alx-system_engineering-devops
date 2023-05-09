73df375ec98e16c555f8df78cfdf9dc459b093d2
moha-mame <>
2023-05-09 12:25:23 +0100

    api_advanced

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"number of subscribers"
+
+import requests
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
