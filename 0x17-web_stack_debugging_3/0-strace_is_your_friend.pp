c08d9ce9af2a46d3df4e9e838169c2cbbc419e04
moha-mame <moha.mame04@gmail.com>
2023-05-10 15:27:36 +0000

    Webstack monitoring

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+#fix the configuration file for Apache web server so that it points to the correct WordPress
+exec { 'fix config file':
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
